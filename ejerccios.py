5d8f0c7d68a4b208da4a28bd231ff5e981d37679
kennyvargasUN <kevargasr@unal.edu.co>
2024-08-12 15:59:18 -0500

    Add files via upload

---
+++ ejerccios.py
@@ -0,0 +1,98 @@
+
+# 1)
+
+def lista_rep (lista):
+
+    for i in lista:
+
+        rep=lista.count(i)
+        if rep > 1 :
+            con = True
+            break
+        else:
+            con = False
+
+
+    if con == True:
+
+        return print("en la lista hay repeticiones de numeros")
+    
+    else:  
+
+        return print("no hay repeticiones en la lista")
+    
+
+lista= [1,2,3,4,5,6,7,8,9,0]
+
+#lista_rep(lista)
+
+# 2)
+
+def vocales (palabra):
+
+    for i in range(0,len(palabra)):
+
+        p = palabra[i]
+        
+        if p.count("a") > 1 or p.count("e") > 1 or p.count("i") > 1 or p.count("o") > 1 or p.count("u") > 1 :
+
+            print(p)
+            break
+
+    if i == len(palabra):
+        print("no existe")
+            
+
+palabra =["los","estudiantes","de","programacion"]
+
+#vocales(palabra)
+
+# 3)
+
+def comparacion (lis1,lis2):
+    res = []
+    for i in range(0,len(lis1)) :
+        m = lis1[i]
+
+        for j in range(0,len(lis2)) :
+
+            if m == lis2[j]:
+                break
+            
+        if j == len(lis2)-1:
+            res.append(m)
+
+    print(res)
+
+lis1 = [1,2,3,4,7]
+lis2 = [1,2,6,5]
+
+#comparacion(lis1,lis2)
+
+# 4)
+
+def promedio (arrglo):
+
+    prom = 0
+
+    for i in range (0,len(arreglo)):
+
+        prom += arreglo[i]
+
+    print(prom/len(arreglo))
+
+arreglo = [1,2,3,4,5,5]
+
+#promedio(arreglo)
+
+# 5)
+
+def medio (arreglo):
+
+    arreglo.sort()
+    posicion = (len(arreglo)-1)/2
+    print(arreglo[int(posicion)])
+
+arreglo2 = [4,6,5,3,7,2,1]
+
+medio(arreglo2)
